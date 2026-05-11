a91827a0b48f6260342e4d09deeca2b7fb3c46da
rylerbleau <73083256+rylerbleau@users.noreply.github.com>
2026-05-11 00:09:57 -0300

    reverted jump, made certain magic numbers into const
    See my TODO in player script for reference on the issue i ran into

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -15,6 +15,10 @@ var is_bouncy: bool = true
 
 var vel_last_tick:Vector3 
 
+# bouncy variables
+const BOUNCY_ACCEL = 30
+const WINDY_MAX_VEL = 5
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -35,8 +39,7 @@ func _physics_process(delta):
 
 	velocity = applied_velocity
 
-	move_and_collide(velocity * delta)
-	
+	move_and_slide()	
 	vel_last_tick = velocity
 
 func handle_controls(delta):
@@ -75,13 +78,10 @@ func handle_gravity(delta):
 	gravity += 25 * delta
 
 	
-	if(is_on_floor() && can_jump == false):
-		# landed this tick
-		# print("landed")
-		can_jump = true
+	
 		
 
-	elif(gravity > 0 and is_on_floor()): 
+	if(gravity > 0 and is_on_floor()): 
 		can_jump = true
 		is_falling = false
 		gravity = 0
@@ -104,51 +104,31 @@ func handle_bouncy_movement(delta, input):
 		
 	else:  # accel
 		movement_velocity = velocity
-		movement_velocity += (30 * input * delta)
+		movement_velocity += (BOUNCY_ACCEL * input * delta)
 		
 	var collision = get_last_slide_collision()
 	if(collision):
-		print("collision")
-	
-		var bounced_vel = velocity.bounce(collision.get_normal().normalized())
-		print("last tick vel: " + str(get_position_delta()) + ", bounced vel: " + str(bounced_vel))
 		
+		# TODO figure out how to do bounces (possibly with move_and_collide())
+		# the issue I was having is that when you check the velocity at the same tick the collision happens, 
+		# it is giving me (i think) the velocity after it collides and zeros out which doesn't help if you want to bounce it
+			
+		var bounced_vel = velocity.bounce(collision.get_normal())
+		print("remainder vel: " + str(collision.get_remainder()) + ", bounced vel: " + str(bounced_vel))
+
 	
 	
 func handle_windy_movement(delta, input):
-	var max_vel = 5
-	
-	if(velocity.length() > max_vel): # over max windy speed: decel
 
+	
+	if(velocity.length() > WINDY_MAX_VEL): # over max windy speed: decel
 		movement_velocity = movement_velocity.lerp(Vector3.ZERO, delta * 10)
 	
 	elif(input.length() <= 0 && movement_velocity.length() > 0): # decel, no input
 		movement_velocity = movement_velocity.lerp(Vector3.ZERO, delta * 50)
 
-
-	
-		
 	else:
-		movement_velocity = 5 * input
-
-	pass
-		
-
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
+		movement_velocity = WINDY_MAX_VEL * input
 
 
 
