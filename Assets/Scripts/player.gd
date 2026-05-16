762bc29214a3099ad0f9a62a67601eec0864fe3d
Paxton Matheson <57409537+tonsofpax@users.noreply.github.com>
2026-05-16 13:58:39 -0300

    Setup GridMap using WIP tile models/meshes.

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -3,10 +3,13 @@ extends CharacterBody3D
 # General variables
 @export var movement_speed = 250
 @export var jump_strength = 10
+@onready var walking_sound = $WalkingSound
+var rng = RandomNumberGenerator.new()
 var gravity = 0
 var movement_velocity: Vector3
 var rotation_direction: float
 var can_jump: bool = true
+var previously_floored: bool = true
 var can_swap_spirits: bool = true
 
 # Bouncy variables
@@ -31,6 +34,7 @@ func _physics_process(delta):
 	# Handlers for player mechanics and gravity mechanics
 	handle_controls(delta)
 	handle_gravity(delta)
+	handle_effects(delta)
 
 	# Specific interactions while Bouncy spirit skill is active
 	
@@ -45,6 +49,7 @@ func _physics_process(delta):
 		if(collision.get_normal().y != 0):
 			gravity = -gravity * 1.5
 		print("collided")
+		SignalBus.play("res://Assets/Sound/Bounce.ogg")
 		is_bouncing = false
 		return
 		
@@ -64,6 +69,11 @@ func _physics_process(delta):
 	velocity = applied_velocity
 
 	move_and_slide()
+	
+	if is_on_floor() and gravity > 2 and !previously_floored:
+		SignalBus.play("res://Assets/Sound/LandPixel.wav")
+
+	previously_floored = is_on_floor()
 
 func handle_controls(delta):
 	
@@ -103,6 +113,7 @@ func handle_controls(delta):
 	if Input.is_action_just_pressed("jump"):
 		if can_jump:
 			jump()
+			SignalBus.play("res://Assets/Sound/JumpPixel.ogg")
 	
 
 func handle_gravity(delta):
@@ -118,6 +129,16 @@ func handle_gravity(delta):
 		is_bouncing = false
 		gravity = 0
 
+func handle_effects(delta):
+	walking_sound.stream_paused = true
+	if is_on_floor():
+		var horizontal_velocity = Vector2(velocity.x, velocity.z)
+		var speed_factor = horizontal_velocity.length() / movement_speed / delta
+		if speed_factor > 0.1: # Likely can tweak this value
+			walking_sound.stream_paused = false
+			walking_sound.pitch_scale = rng.randf_range(0.8,1.1)
+			walking_sound.volume_db = -2 - (1/speed_factor)
+
 func jump():
 	gravity = -jump_strength
 
@@ -130,7 +151,6 @@ func handle_bouncy_movement(delta, input):
 	
 	if(input.length() <= 0 && movement_velocity.length() > 0): # decel, no input
 		movement_velocity = movement_velocity.lerp(Vector3.ZERO, delta * 1)
-		
 	else:  # accel
 		movement_velocity = BOUNCY_ACCEL * input
 
@@ -159,6 +179,7 @@ func use_skill():
 		is_dashing = true
 		can_dash = false
 		SignalBus.emit_signal("dash_cooldown_start", $dash_cooldown.wait_time)
+		SignalBus.play("res://Assets/Sound/Dash.wav")
 		$dash_timer.start()
 		$dash_cooldown.start()
 		print("Skill use Zoom")
