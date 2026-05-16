c1669c563f1032551e27324e4192738633d05ad7
Paxton Matheson <57409537+tonsofpax@users.noreply.github.com>
2026-05-16 13:58:39 -0300

    Setup GridMap using WIP tile models/meshes.

--- Assets/Scripts/signal_bus.gd
+++ Assets/Scripts/signal_bus.gd
@@ -7,6 +7,33 @@ signal swap_cooldown_start(val)
 signal dash_cooldown_start(val)
 signal bounce_cooldown_start(val)
 
+var num_streams = 8
+var bus = "sfx"
+
+var available = []  # The available streams to use
+var queue = []  # The queue of sounds to play
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(ready)
+	for i in num_streams:
+		var p = AudioStreamPlayer.new()
+		add_child(p)
+		
+		available.append(p)
+		
+		p.volume_db = -10
+		p.finished.connect(_on_stream_finished.bind(p))
+		p.bus = bus
+
+func _on_stream_finished(stream): available.append(stream)
+
+func play(sound_path): queue.append(sound_path)
+
+func _process(_delta):
+	if not queue.is_empty() and not available.is_empty():
+		
+		available[0].stream = load(queue.pop_front())
+		available[0].play()
+		available[0].pitch_scale = randf_range(0.9, 1.1)
+		
+		available.pop_front()
